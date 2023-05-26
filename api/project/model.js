1769283bb0f865d9f00a82c37b39ca6778c1f16e
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-05-26 00:07:31 -0400

    checkpoint

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,17 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+
+async function getProjects () {
+    return db('projects')
+}
+
+async function postProject (project) {
+    const [ project_id ] = await db('projects').insert(project)
+    return db('projects').where({ project_id }).first()    
+}
+
+module.exports = { 
+    getProjects,
+    postProject
+}
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+// require('dotenv').config()
+
+const server = require('./api/server.js')
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => console.log(`listening on port: ${port}`))
