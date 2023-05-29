6b76f3a1d165110a46d311db75df626f6d8ae118
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-05-29 19:00:30 -0400

    almost all tests passing

--- data/migrations/20230529222400_initial.js
+++ data/migrations/20230529222400_initial.js
@@ -1,26 +1,46 @@
-
-exports.up = async function(knex) {
-  await knex.schema
-    .createTable('projects', table => {
-        table.increments('project_id')
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments('project_id')
+        tbl.string('project_name', 200).notNullable()
+        tbl.string('project_description', 200)
+        tbl.boolean('project_completed').defaultTo(0)
     })
-    .createTable('resources', table => {
-        table.increments('resource_id')
+    .createTable('resources', tbl => {
+        tbl.increments('resource_id')
+        tbl.string('resource_name').notNullable().unique()
+        tbl.string('resource_description', 200)
     })
-    .createTable('tasks', table => {
-        table.increments('task_id')
+    .createTable('tasks', tbl => {
+        tbl.increments('task_id')
+        tbl.string('task_description', 200)
+        tbl.string('task_notes', 200)
+        tbl.boolean('task_completed').defaultTo(0)
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
     })
-    .createTable('project_resources', table => {
-
+    .createTable('project_resources', tbl => {
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')    
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
     })
 };
 
 
-exports.down = async function(knex) {
-  await knex.schema
+exports.down = function(knex) {
+  return knex.schema
     .dropTableIfExists('project_resources')
     .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects')
-
 };
